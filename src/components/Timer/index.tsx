c55ebbb566923d85f30d5be1f1abee8719d010e3
marciano.ns <marciano.ns@bolsista.pti.org.br>
2024-10-21 16:25:30 -0300

    feat: Timer component

---
+++ src/components/Timer/index.tsx
@@ -0,0 +1,19 @@
+import Button from "../Button";
+import Clock from "./Clock";
+import style from './Timer.module.scss'
+
+export default function Timer(){
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>
+        Escolha um card e inicie o Cronômetro
+      </p>
+      <div className={style.relogioWrapper}>
+        <Clock />
+      </div>
+      <Button>
+        Começar
+      </Button>
+    </div>
+  )
+}
--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Formulario from '../Form';
 import List from '../List';
 import style from './App.module.scss'
+import Timer from '../Timer';
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Formulario />
       <List />
+      <Timer />
     </div>
   );
 }
