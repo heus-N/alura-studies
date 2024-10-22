3c7b687b3cd5a20258626e09ac5655d9cbac0688
marciano.ns <marciano.ns@bolsista.pti.org.br>
2024-10-22 09:24:17 -0300

    feat: time in seconds function

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,8 +1,10 @@
 import Button from "../Button";
 import Clock from "./Clock";
 import style from './Timer.module.scss'
+import {timeForSeconds} from '../../common/utils/date'
 
 export default function Timer(){
+  console.log('conversão: ', timeForSeconds('01:01:01'))
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>
