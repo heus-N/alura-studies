9c8fd013e92bf984341c4e40c0e89e61c1538c13
marciano.ns <marciano.ns@bolsista.pti.org.br>
2024-10-22 11:32:15 -0300

    fix: button, form functionComponents

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import style from './Button.module.scss'
 
-class Button extends React.Component<{children: any, type?: "submit" | "reset" | "button" | undefined, onClick?: () => void }> {
-  render(){
-    const {type = "button", onClick} = this.props
-    return (
-      <button onClick={onClick} type={type} className={style.botao} >
-        {this.props.children}
-      </button>
-    )
-  }
+interface Props{
+  children?: any, 
+  type?: "submit" | "reset" | "button" | undefined, 
+  onClick?: () => void
+}
+
+function Button({onClick, type, children} : Props) {
+  return (
+    <button onClick={onClick} type={type} className={style.botao} >
+      {children}
+    </button>
+  )
 }
 
 export default Button
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,64 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import style from './Form.module.scss'
 import { ITask } from "../../types/tasks";
 import {v4 as uuidv4} from 'uuid'
 
-class Formulario extends React.Component<{setTasks: React.Dispatch<React.SetStateAction<ITask[]>>}>{
-  state = {
-    tarefa: '',
-    tempo: '00:00'
-  }
 
-  addTask(e: React.FormEvent<HTMLFormElement>){
+interface Props{
+  setTasks: React.Dispatch<React.SetStateAction<ITask[]>>
+}
+
+function Formulario({ setTasks } : Props){
+  const [tarefa, setTarefa] = useState("")
+  const [tempo, setTempo] = useState("00:00")
+
+  function addTask(e: React.FormEvent<HTMLFormElement>){
     e.preventDefault();
-    this.props.setTasks(prevTasks => [...prevTasks, 
-      {...this.state, 
+    setTasks(prevTasks => 
+      [
+        ...prevTasks,
+      {
+        tarefa,
+        tempo,
         id: uuidv4(),
         selecionado: false, 
         completado: false
       }])
-    this.setState({
-      tarefa: '',
-      tempo: '00:00'
-    })
+    setTarefa("")
+    setTempo("00:00")
   }
 
-  render(){
-    return(
-      <form className={style.novaTarefa} onSubmit={this.addTask.bind(this)}>
-        <div className={style.inputContainer}>
-          <label htmlFor="tarefa">Tarefa</label>
-          <input 
-            type="text" 
-            name="tarefa"
-            value={this.state.tarefa}
-            onChange={(e) => {this.setState({...this.state, tarefa: e.target.value})}}
-            id="tarefa"
-            placeholder="O que você quer estudar"
-            required
-          />
-        </div>
-        <div className={style.inputContainer}>
-          <label htmlFor="tempo">Tempo</label>
-          <input 
-            type="time" 
-            step="1"
-            name="tempo"
-            value={this.state.tempo}
-            onChange={(e) => {this.setState({...this.state, tempo: e.target.value})}}
-            id="tempo"
-            min="00:00:00"
-            max="01:30:00"
-            required
-          />
-        </div>
-        <Button type="submit">
-          Adicionar
-        </Button>
-      </form>
-    )
-  }
+  return(
+    <form className={style.novaTarefa} onSubmit={addTask}>
+      <div className={style.inputContainer}>
+        <label htmlFor="tarefa">Tarefa</label>
+        <input 
+          type="text" 
+          name="tarefa"
+          value={tarefa}
+          onChange={(e) => setTarefa( e.target.value )}
+          id="tarefa"
+          placeholder="O que você quer estudar"
+          required
+        />
+      </div>
+      <div className={style.inputContainer}>
+        <label htmlFor="tempo">Tempo</label>
+        <input 
+          type="time" 
+          step="1"
+          name="tempo"
+          value={tempo}
+          onChange={(e) => setTempo(e.target.value)}
+          id="tempo"
+          min="00:00:00"
+          max="01:30:00"
+          required
+        />
+      </div>
+      <Button type="submit">
+        Adicionar
+      </Button>
+    </form>
+  )
 }
 
 export default Formulario
